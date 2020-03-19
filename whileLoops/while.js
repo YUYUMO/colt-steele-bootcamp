aefe2f00a28d64f8815b9ea4908819141a9e3222
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-03-19 17:37:53 +0000

    complete javascript  while loops exercise

---
+++ whileLoops/while.js
@@ -0,0 +1,29 @@
+var number = -10
+while(number <= 19) {
+  console.log(number);
+  number++;
+}
+
+var number = 10
+while(number <= 40){
+  if(number % 2 === 0){
+  console.log(number);
+  }
+  number++;
+}
+
+var number = 300
+while (number <= 333){
+  if(number % 2 !== 0){
+    console.log(number);
+  }
+  number++;
+}
+
+var number = 5
+while (number <= 50){
+  if(number % 5 === 0 && number % 3 === 0){
+    console.log(number);
+  }
+  number++;
+}
