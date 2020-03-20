a85f869c66d4ff9a55be02668f6cd7d7cdac7c20
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-03-20 11:54:59 +0000

    complete javascript annoy-omatic exercise

---
+++ annoy-omatic/annoy.js
@@ -0,0 +1,16 @@
+var response = prompt("Are we there yet?");
+
+while (!response.includes("yeah") && !response.includes("yes")){
+  var response = prompt("Are we there yet?");
+}
+
+alert ("Yay, we made it!");
+
+version 2
+var response = prompt("Are we there yet?");
+
+while (response.indexOf("yes") === -1){
+  var response = prompt("Are we there yet?");
+}
+
+alert ("Yay, we made it!");
