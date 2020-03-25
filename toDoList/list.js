7c266b20eb291bfaa147913de20f89521cb86072
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-03-25 17:14:32 +0000

    added delete function to the todos app and refactor the code

--- toDoList/list.js
+++ toDoList/list.js
@@ -1,17 +1,17 @@
 //set up a variable to store to-dos
-var todos = ["Buy a new laptop"];
+var todos = ["Buy Ring Fit Switch Game"];
 var input = prompt("What would you like to do?");
 window.setTimeout(function() {
   // ask user for input until user types quit
   while(input!== "quit" ){
     //if input is "list", list all todos
     if (input=== "list"){
-      console.log(todos);
+      printList();
     //if input is new, ask for a new todo
     } else if(input==="new"){
-        var newTodo = prompt("Enter a new todo");
-      // add the new todo to the list
-        todos.push(newTodo);
+        addTodo();
+    } else if(input==="delete"){
+        deleteTodo();
     }
     // ask user for input again;
     input = prompt("What would you like to do?");
@@ -20,5 +20,24 @@ window.setTimeout(function() {
   console.log("You have quit the app");
 }, 500);
 
+function printList(){
+  console.log ("**********");
+  todos.forEach(function(todo,i){
+    console.log (i + "." + todo);
+  });
+  console.log ("**********");
+}
 
+function addTodo(){
+  var newTodo = prompt("Enter a new todo");
+  // add the new todo to the list
+  todos.push(newTodo);
+  console.log(newTodo + " added to the list");
+}
+
+function deleteTodo(){
+  var index = prompt("Enter the index of todo you would like to delete");
+  todos.splice(index, 1);
+  console.log("Todo removed");
+}
 
