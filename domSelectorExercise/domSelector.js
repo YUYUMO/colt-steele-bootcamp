5cd44eab9025f223fbad9396cc735b3429b7ae9d
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-03-31 17:16:06 +0100

    complete DOM selector exercise

---
+++ domSelectorExercise/domSelector.js
@@ -0,0 +1,8 @@
+// to select the first p
+var tag = document.getElementById("first");
+var tag = document.querySelector("#first");
+var tag = document.getElementsByClassName("special")[0];
+var tag = document.querySelector(".special");
+var tag = document.getElementsByTagName("p")[0];
+var tag = document.querySelector("p");
+var tag = document.querySelector("h1 + p");
