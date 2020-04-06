b7e4b9e3bee03a97b1298bbd299f26574df68134
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-04-06 14:25:45 +0100

    complete JS events exercise with to do list

---
+++ toDoListEvents/toDoEvents.js
@@ -0,0 +1,21 @@
+// var firstLi = document.querySelector("li");
+// firstLi.addEventListener("mouseover", function(){
+//   firstLi.style.color = "blue";
+// });
+// firstLi.addEventListener("mouseout", function(){
+//   firstLi.style.color = "black";
+// });
+
+var lis = document.querySelectorAll("li");
+
+for(var i = 0; i < lis.length; i++){
+  lis[i].addEventListener("mouseover", function(){
+    this.classList.add("selected");
+  });
+  lis[i].addEventListener("mouseout", function(){
+    this.classList.remove("selected");
+  });
+  lis[i].addEventListener("click", function(){
+    this.classList.toggle("done");
+  });
+}
