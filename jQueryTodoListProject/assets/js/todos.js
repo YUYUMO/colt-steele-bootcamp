82f8cf8426505850cd677609f7c6a17a7a7ba16d
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-04-20 17:26:04 +0100

    add function which enables todos to be faded out and deleted

--- jQueryTodoListProject/assets/js/todos.js
+++ jQueryTodoListProject/assets/js/todos.js
@@ -2,3 +2,14 @@
 $("li").click(function(){
   $(this).toggleClass("completed");
 });
+
+// click on X to delete toDo
+$("span").click(function(event){
+  // $(this).parent refers to the li
+  $(this).parent().fadeOut(500, function(){
+    // $(this) refers to the li. It's only removed after fade out happens
+    $(this).remove();
+  });
+  // stop event bubbling
+  event.stopPropagation();
+});
