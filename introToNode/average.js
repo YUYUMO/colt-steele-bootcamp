71c12f33e8f09321792c3b8ac4a9aad1446906f0
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-04-24 21:11:30 +0100

    change console.log to return. Add console.log to print the result

--- introToNode/average.js
+++ introToNode/average.js
@@ -4,11 +4,11 @@ function average(array){
     sum += array[i];
   }
   var average = Math.round(sum/array.length);
-  console.log(average);
+  return(average);
 }
 
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores);
+console.log(average(scores));
 
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2);
+console.log(average(scores2));
