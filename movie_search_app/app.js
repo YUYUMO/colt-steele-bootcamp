cfa326ae1693854e6ec39d2d6fef5f3449767d59
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-04-30 18:17:23 +0100

    Add search route for the movies API app

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -5,8 +5,15 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.render("search");
+});
+
 app.get("/results", function(req, res){
-  request('http://www.omdbapi.com/?s=london&apikey=thewdb', function(error, response, body){
+  // search is the name of the form field
+  var query = req.query.search;
+  var url = 'http://www.omdbapi.com/?s=' + query + '&apikey=thewdb';
+  request(url, function(error, response, body){
     if(!error && response.statusCode == 200){
       var data = JSON.parse(body)
       res.render("results",{data:data});
