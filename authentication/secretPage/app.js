8a07dd7901b84cba2fd4dcb7dd390504c4513f54
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-18 22:49:25 +0100

    debug app.js

--- authentication/secretPage/app.js
+++ authentication/secretPage/app.js
@@ -1,22 +1,41 @@
 var express = require("express");
-var app = express();
 var mongoose = require("mongoose");
+var passport = require("passport");
+var bodyParser = require("body-parser");
+var User = require("./models/user");
+var LocalStrategy = require("passport-local");
+var passportLocalMongoose = require("passport-local-mongoose")
 
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost:27017/secretPage', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/secretPage', { useNewUrlParser: true }); 
 
-app.set('view engine','ejs');
+var app = express();
+app.set('view engine', 'ejs');
+app.use(require("express-session")({
+  secret: "Rusty is the best and cutest dog in the world",
+  resave: false,
+  saveUninitialized: false
+}));
 
-//secret route
-app.get("/secret", function(req,res){
-  res.render("secret");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//============
+// ROUTES
+//============
+
+app.get("/", function(req, res){
+    res.render("home");
 });
 
-// root route
-app.get("/", function(req,res){
-  res.render("home");
-})
+app.get("/secret",function(req, res){
+   res.render("secret"); 
+});
+
 
 app.listen(3000, function(){
-  console.log("Server has started!!");
-});
+  console.log("server started.......");
+})
