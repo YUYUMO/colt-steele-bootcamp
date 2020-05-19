99fc53e37cd8a4f1ab921f659b47d48b3d10d0da
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-19 15:05:10 +0100

    add sign up form, route to show sign up form, route to handle user sign up

--- authentication/secretPage/app.js
+++ authentication/secretPage/app.js
@@ -11,6 +11,7 @@ mongoose.connect('mongodb://localhost:27017/secretPage', { useNewUrlParser: true
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(require("express-session")({
   secret: "Rusty is the best and cutest dog in the world",
   resave: false,
@@ -35,6 +36,24 @@ app.get("/secret",function(req, res){
    res.render("secret"); 
 });
 
+// Auth routes
+// show sign up form 
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+//handling user sign up
+app.post("/register", function(req, res){
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          return res.render('register');
+      }
+      passport.authenticate("local")(req, res, function(){
+         res.redirect("/secret");
+      });
+  });
+});
 
 app.listen(3000, function(){
   console.log("server started.......");
