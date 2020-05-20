d11cd6204e4853a038052876a75271993f075b56
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-20 16:43:36 +0100

    add logout route and isLoggedIn middleware

--- authentication/secretPage/app.js
+++ authentication/secretPage/app.js
@@ -33,7 +33,7 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret",function(req, res){
+app.get("/secret",isLoggedIn,function(req, res){
    res.render("secret"); 
 });
 
@@ -68,7 +68,18 @@ app.post("/login", passport.authenticate("local", {
   failureRedirect: "/login"
 }) ,function(req, res){
 });
+// logout route
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+})
 
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(3000, function(){
   console.log("server started.......");
