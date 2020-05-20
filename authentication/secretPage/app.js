e4f430af7f74512cdb3a857dd74e8173f53aefe7
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-20 10:20:21 +0100

    add login routes and login form. Configure passport.js

--- authentication/secretPage/app.js
+++ authentication/secretPage/app.js
@@ -21,6 +21,7 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -54,6 +55,20 @@ app.post("/register", function(req, res){
       });
   });
 });
+
+//LOGIN routes
+//render login form
+app.get("/login",function(req,res){
+  res.render("login");
+});
+// login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/secret",
+  failureRedirect: "/login"
+}) ,function(req, res){
+});
+
 
 app.listen(3000, function(){
   console.log("server started.......");
