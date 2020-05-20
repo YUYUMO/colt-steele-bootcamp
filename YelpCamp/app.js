38e08c959c67a3d1e37fc6085d4fb01193fd6302
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-20 21:59:45 +0100

    add user model, install packages for authentication, configure passport, add register routes & add register template

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,8 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 var Campground = require("./models/campground");
 var Comment = require("./models/comment");
+var User = require("./models/user");
 var seedDB = require("./seeds")
 
 mongoose.set('useUnifiedTopology', true);
@@ -15,6 +18,18 @@ app.use(express.static(__dirname + "/public"));
 // console.log(__dirname);
 seedDB();
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+  secret: "Once again Rusty wins cutest dog!",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Campground.create(
 //   {
@@ -137,6 +152,28 @@ app.post("/campgrounds/:id/comments", function(req,res){
   })
 })
 
+//  ===========
+// AUTH ROUTES
+//  ===========
+
+// show register form
+app.get("/register", function(req, res){
+  res.render("register"); 
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+      if(err){
+          console.log(err);
+          return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function(){
+         res.redirect("/campgrounds"); 
+      });
+  });
+});
 
 app.listen(3000, function(){
   console.log("Server has started!!");
