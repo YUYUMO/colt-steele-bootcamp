81c519af84a7e4cd5dd721d7cec16934d2cc7cfa
Phyllis Yu Yu Mo <phyllis.yymo@gmail.com>
2020-05-21 17:26:09 +0100

    Prevent an unauthenticated user from creating a campground. Save username and ID to newly created campground and display the username on the show page.

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -15,25 +15,30 @@ router.get("/", function(req, res){
 });
 
 // CREATE route - add new dog to Database
-router.post("/", function(req,res){
+router.post("/", isLoggedIn, function(req,res){
   // get data from form and add to campgrounds array (body for parsing)
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc}
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  }
+  var newCampground = {name: name, image: image, description: desc, author:author}
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated){
     if(err){
       console.log(err);
     } else {
         // redirect to campgrounds page
-      res.redirect("/campgrounds");
+        // console.log(newlyCreated);
+        res.redirect("/campgrounds");
     }
   });
 });
 
 //NEW route - display form to create new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
   res.render("campgrounds/new");
 });
 
@@ -50,5 +55,13 @@ router.get("/:id", function(req,res){
     }
   });
 });
+
+// isLoggedIn middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+      return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
